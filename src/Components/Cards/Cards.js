f60395175aa61de9daffad12a51cc337e068b47a
abdullah al emon <abdullahemon905@gmail.com>
2022-10-14 17:42:05 +0600

    Css  Section Added

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -5,7 +5,7 @@ import Card from '../Card/Card';
 const Cards = () => {
     const cards = useLoaderData().data
     return (
-        <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 mx-6 md:mx-20 my-40'>
+        <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 mx-6 md:mx-20 md:my-40 my-20'>
             {
                 cards.map(card => <Card card={card} key={card.id}></Card>)
             }
--- src/Components/Questions/Questions.js
+++ src/Components/Questions/Questions.js
@@ -7,12 +7,12 @@ const Questions = ({ ques }) => {
     // console.log(ques)
     const { correctAnswer, options, question } = ques;
     return (
-        <div className='bg-slate-200 my-8 py-3 px-8 w-[80%] mx-auto rounded-2xl '>
+        <div className='bg-slate-200 my-8 md:py-3 py-1 md:px-8 px-4 w-[80%] mx-auto rounded-2xl '>
             <Question
                 question={question}
                 correctAnswer={correctAnswer}
             ></Question>
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-5 pb-3'>
+            <div className='grid grid-cols-1 md:grid-cols-2 md:gap-5 gap-2 pb-3'>
                 {
                     options.map(option => <Option
                         option={option}
